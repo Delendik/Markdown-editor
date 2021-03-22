85e84d01c545b65cd29d2607c2ebccb5e5a09ae4
Alexey Delendik <delendikalexey@gmail.com>
2021-03-22 17:31:54 +0300

    editor ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import marked from 'marked';
 import './App.css';
 
 function App() {
+  const [markdown, setMarkdown] = useState('');
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea onChange = {handleChange} />
+      <div 
+        className="preview"
+        dangerouslySetInnerHTML={{__html: marked(markdown)}} 
+      />
     </div>
   );
 }
